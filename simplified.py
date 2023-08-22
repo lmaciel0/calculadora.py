bb38c34d6c475bdc8f5367e71fbc8876b27b46d9
Roberto Lucas Maciel De Mendonça <lucasmaciel900@gmail.com>
2023-08-22 10:07:16 -0300

    more simplified
    functional, lighter, but without aesthetics

--- simplified.py
+++ simplified.py
@@ -1,61 +1,60 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 
-def botao_click(numero):
-    global numero_1, numero_2
-    numero_1 = numero_1 + str(numero)
-    label_text.set(numero_1)
+# Crie a janela principal
+janela = tk.Tk()
+janela.title("Calculadora")
 
-def botao_igual():
-    global numero_1, numero_2
-    numero_2 = label_text.get()
-    result = int(numero_1) + int(numero_2)
-    label_text.set(result)
+# Defina o estilo dos botões
+estilo_botoes = ttk.Style()
+estilo_botoes.configure("Estilo.TButton", font=("Helvetica", 16))
 
-def botao_limpa():
-    global numero_1, numero_2
-    numero_1 = ""
-    numero_2 = ""
-    label_text.set("")
+# Crie o campo de entrada
+entrada = ttk.Entry(janela, font=("Helvetica", 20), justify="right")
+entrada.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-root = Tk()
-root.title("Calculadora")
-root.geometry("408x355")
+# Função para inserir números e operadores no campo de entrada
+def inserir_texto(valor):
+    entrada.insert("end", valor)
 
-numero_1 = ""
-numero_2 = ""
+# Função para limpar o campo de entrada
+def limpar():
+    entrada.delete(0, "end")
 
-label_text = Label(root, text="0", font=("Helvetica", 20))
-label_text.grid(row=0, column=0, columnspan=4)
+# Função para calcular
+def calcular():
+    try:
+        expressao = entrada.get()
+        resultado = eval(expressao)
+        entrada.delete(0, "end")
+        entrada.insert("end", resultado)
+    except:
+        entrada.delete(0, "end")
+        entrada.insert("end", "Erro")
 
-# Botões de números
-um = Button(root, text="1", command=lambda: botao_click(1))
-um.grid(row=1, column=0)
-dois = Button(root, text="2", command=lambda: botao_click(2))
-dois.grid(row=1, column=1)
-tres = Button(root, text="3", command=lambda: botao_click(3))
-tres.grid(row=1, column=2)
+# Crie os botões de números e operadores
+botoes = [
+    '1', '2', '3', '/',
+    '4', '5', '6', '*',
+    '7', '8', '9', '-',
+    '0', 'C', '='
+]
 
-quatro = Button(root, text="4", command=lambda: botao_click(4))
-quatro.grid(row=2, column=0)
-cinco = Button(root, text="5", command=lambda: botao_click(5))
-cinco.grid(row=2, column=1)
-seis = Button(root, text="6", command=lambda: botao_click(6))
-seis.grid(row=2, column=2)
+linha = 1
+coluna = 0
+for texto in botoes:
+    if texto == '0':
+        ttk.Button(janela, text=texto, style="Estilo.TButton", command=lambda num=texto: inserir_texto(num)).grid(row=linha, column=coluna, columnspan=2, padx=5, pady=5)
+        coluna += 2
+    elif texto == '=' or texto == 'C':
+        ttk.Button(janela, text=texto, style="Estilo.TButton", command=calcular if texto == '=' else limpar).grid(row=linha, column=coluna, padx=5, pady=5)
+        coluna += 1
+    else:
+        ttk.Button(janela, text=texto, style="Estilo.TButton", command=lambda num=texto: inserir_texto(num)).grid(row=linha, column=coluna, padx=5, pady=5)
+        coluna += 1
+    if coluna > 3:
+        coluna = 0
+        linha += 1
 
-sete = Button(root, text="7", command=lambda: botao_click(7))
-sete.grid(row=3, column=0)
-oito = Button(root, text="8", command=lambda: botao_click(8))
-oito.grid(row=3, column=1)
-nove = Button(root, text="9", command=lambda: botao_click(9))
-nove.grid(row=3, column=2)
-
-zero = Button(root, text="0", command=lambda: botao_click(0))
-zero.grid(row=4, column=0)
-
-limpa = Button(root, text="C", command=botao_limpa)
-limpa.grid(row=4, column=1)
-
-igual = Button(root, text="=", command=botao_igual)
-igual.grid(row=4, column=2)
-
-root.mainloop()
+# Inicie a interface gráfica
+janela.mainloop()
